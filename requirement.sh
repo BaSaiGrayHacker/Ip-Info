57e62c018cfea81b601c59cbe699737cb7f62698
BaSaiGrayHacker <49263443+BaSaiGrayHacker@users.noreply.github.com>
2019-04-07 04:51:14 +0000

    Add files via upload

---
+++ requirement.sh
@@ -0,0 +1,28 @@
+#!/bin/python
+# 
+#
+# Name	:	IP Info
+# Code		:	python
+# facebook  :   https://www.facebook.com/ba.sai.LeeLeelrrlrr
+# github	:	github.com/Bhai4You
+#
+#
+#	<==(__WARNING__)==>
+#
+# MODIFY WITHOUT PERMISSION !!!
+#
+#	<==(__WARNING__)==>
+#
+#
+#
+apt update
+apt upgrade 
+apt install figlet -y
+apt install toilet -y
+apt install nano -y
+apt install ruby -y
+apt install curl -y
+gem install lolcat
+clear
+toilet -f pagga "Done..!!" | lolcat
+
